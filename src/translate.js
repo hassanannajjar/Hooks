c5bf24435b01f95902583feb0f8fc9ac9961e01d
ha77an <ha77an19@gmail.com>
2020-04-29 13:47:16 +0300

    added api and axios

--- src/translate.js
+++ src/translate.js
@@ -12,7 +12,6 @@ const Translate = () => {
           `https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20200428T021732Z.a47b9c7034e7647c.a40b1a8a1d0d372b1e84c9f666ebc5cf19a0fc65&text=${inputText}&lang=ar`
         )
         .then(({ data }) => {
-          console.log("res");
           setOutText(data.text[0]);
         })
         .catch((err) => {
